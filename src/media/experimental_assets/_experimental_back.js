b94ca3f4638afeb865f46c1614a87f28943505e5
harkusha <ext-illia.harkusha@here.com>
2024-07-29 18:15:50 +0300

    Green/Red switch implemented + scripting code readability changes
    - For maximum flexibility introduced ability to disable correctly selected green region highlighting via card template
    - JS code moved out into separate files for front and back card templates
    - Each file is wrapped into IIFE to avoid re-declaring variables when switching between cards (Anki uses persistent webview)

---
+++ src/media/experimental_assets/_experimental_back.js
@@ -0,0 +1,35 @@
+(function () {
+  let regionCode = sessionStorage.getItem("regionCode");
+
+  // Green/Red region highlighting color depending on user answer
+  let color = Boolean(Number(sessionStorage.getItem("greenRedRegionEnabled")))
+                      && regionCode === sessionStorage.getItem("selectedRegion")
+                        ? "#329446" // green if both enabled and region selected is correct
+                        : "#c02637"; // red otherwise
+
+  new jsVectorMap({
+    selector: "#map-back",
+    map: "world",
+    zoomButtons: false,
+    selectedRegions: [regionCode],
+    backgroundColor: "#b3dff5",
+
+    focusOn: {
+      region: regionCode,
+      animate: true
+    },
+    regionStyle: {
+      initial: {
+        fill: "#fdfbe5",
+        stroke: "#757674",
+        strokeWidth: 1
+      },
+      selected: {fill: color}
+    },
+
+    onRegionTooltipShow(event, tooltip) {
+      tooltip._tooltip.style["background-color"] = "#fdfbe5";
+      tooltip._tooltip.style["color"] = "black";
+    }
+  });
+}())
---
+++ src/media/experimental_assets/_experimental_front.js
@@ -0,0 +1,53 @@
+// IIFE used specifically to isolate namespaces between cards
+(function () {
+  // Tooltip on mobile is required for dragging to work
+  // To avoid hinting user, prevent tooltip from being shown by disabling it via style
+  let mobileHack =
+    navigator.userAgent.indexOf("Mobile") > 0
+      ? {
+        showTooltip: true,
+        onRegionTooltipShow(event, tooltip) {
+          tooltip._tooltip.style.display = "none";
+        }
+      }
+      : {showTooltip: false};
+
+  // Set event handler to swap card to answer side on "Enter" press
+  document.querySelector("textarea#typeans").onkeypress = () => _typeAnsPress();
+
+  new jsVectorMap({
+    selector: "#map-front",
+    map: "world",
+    zoomButtons: false,
+    backgroundColor: "#b3dff5",
+    regionsSelectable: true,
+    regionsSelectableOne: true,
+
+    regionStyle: {
+      initial: {
+        fill: "#fdfbe5",
+        stroke: "#757674",
+        strokeWidth: 1
+      },
+      selected: {fill: "#e7f3ea"}
+    },
+
+    onRegionSelected(code) {
+      // Persist selected region for Green/Red logic (see Back Template)
+      sessionStorage.setItem("selectedRegion", code);
+
+      // Show answer hack
+      //AnkiDroid requires use of API instead of event dispatch
+      if (typeof AnkiDroidJS !== "undefined") {
+        showAnswer();
+      } else {
+        // Simulate pressing "Enter" on the input element to show the answer
+        let input = document.querySelector("textarea#typeans");
+        let ev = new KeyboardEvent("keypress", {code: "Enter"});
+        input.dispatchEvent(ev);
+      }
+    },
+
+    ...mobileHack
+  });
+}())
