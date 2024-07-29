cb60b67692fe853006018fce7f03aefafc9ea960
harkusha <ext-illia.harkusha@here.com>
2024-07-29 19:01:53 +0300

    Interactive map / static fallback manual switch implemented
    - In case of interactive map trouble to prevent user from need to reimport normal UG deck implemented logic to make it possible to disable interactive map
    - In case of being disabled it gets replaced with static fallback as if the deck is of Extended type

--- src/media/experimental_assets/_experimental_back.js
+++ src/media/experimental_assets/_experimental_back.js
@@ -1,35 +1,49 @@
+// IIFE used specifically to isolate namespaces between cards
 (function () {
-  let regionCode = sessionStorage.getItem("regionCode");
+  if (Number(sessionStorage.getItem("interactiveEnabled")))
+    interactiveMapMode();
+  else
+    staticMapFallbackMode();
 
-  // Green/Red region highlighting color depending on user answer
-  let color = Boolean(Number(sessionStorage.getItem("greenRedRegionEnabled")))
-                      && regionCode === sessionStorage.getItem("selectedRegion")
-                        ? "#329446" // green if both enabled and region selected is correct
-                        : "#c02637"; // red otherwise
+  function interactiveMapMode() {
+    document.querySelector(".value--map").style.display = "block";
 
-  new jsVectorMap({
-    selector: "#map-back",
-    map: "world",
-    zoomButtons: false,
-    selectedRegions: [regionCode],
-    backgroundColor: "#b3dff5",
+    let regionCode = sessionStorage.getItem("regionCode");
 
-    focusOn: {
-      region: regionCode,
-      animate: true
-    },
-    regionStyle: {
-      initial: {
-        fill: "#fdfbe5",
-        stroke: "#757674",
-        strokeWidth: 1
+    // Green/Red region highlighting color depending on user answer
+    let color = Boolean(Number(sessionStorage.getItem("greenRedRegionEnabled")))
+    && regionCode === sessionStorage.getItem("selectedRegion")
+      ? "#329446" // green if both enabled and region selected is correct
+      : "#c02637"; // red otherwise
+
+    new jsVectorMap({
+      selector: "#map-back",
+      map: "world",
+      zoomButtons: false,
+      selectedRegions: [regionCode],
+      backgroundColor: "#b3dff5",
+
+      focusOn: {
+        region: regionCode,
+        animate: true
       },
-      selected: {fill: color}
-    },
+      regionStyle: {
+        initial: {
+          fill: "#fdfbe5",
+          stroke: "#757674",
+          strokeWidth: 1
+        },
+        selected: {fill: color}
+      },
 
-    onRegionTooltipShow(event, tooltip) {
-      tooltip._tooltip.style["background-color"] = "#fdfbe5";
-      tooltip._tooltip.style["color"] = "black";
-    }
-  });
+      onRegionTooltipShow(event, tooltip) {
+        tooltip._tooltip.style["background-color"] = "#fdfbe5";
+        tooltip._tooltip.style["color"] = "black";
+      }
+    });
+  }
+
+  function staticMapFallbackMode() {
+    document.querySelector(".value--image").style.display = "block";
+  }
 }())
--- src/media/experimental_assets/_experimental_front.js
+++ src/media/experimental_assets/_experimental_front.js
@@ -1,9 +1,16 @@
 // IIFE used specifically to isolate namespaces between cards
 (function () {
-  // Tooltip on mobile is required for dragging to work
-  // To avoid hinting user, prevent tooltip from being shown by disabling it via style
-  let mobileHack =
-    navigator.userAgent.indexOf("Mobile") > 0
+  if (Number(sessionStorage.getItem("interactiveEnabled")))
+    interactiveMapMode();
+  else
+    staticMapFallbackMode();
+
+  function interactiveMapMode() {
+    document.querySelector(".value--map").style.display = "block";
+
+    // Tooltip on mobile is required for dragging to work
+    // To avoid hinting user, prevent tooltip from being shown by disabling it via style
+    let mobileHack = navigator.userAgent.indexOf("Mobile") > 0
       ? {
         showTooltip: true,
         onRegionTooltipShow(event, tooltip) {
@@ -12,42 +19,47 @@
       }
       : {showTooltip: false};
 
-  // Set event handler to swap card to answer side on "Enter" press
-  document.querySelector("textarea#typeans").onkeypress = () => _typeAnsPress();
+    // Set event handler to swap card to answer side on "Enter" press
+    document.querySelector("textarea#typeans").onkeypress = () => _typeAnsPress();
 
-  new jsVectorMap({
-    selector: "#map-front",
-    map: "world",
-    zoomButtons: false,
-    backgroundColor: "#b3dff5",
-    regionsSelectable: true,
-    regionsSelectableOne: true,
+    new jsVectorMap({
+      selector: "#map-front",
+      map: "world",
+      zoomButtons: false,
+      backgroundColor: "#b3dff5",
+      regionsSelectable: true,
+      regionsSelectableOne: true,
 
-    regionStyle: {
-      initial: {
-        fill: "#fdfbe5",
-        stroke: "#757674",
-        strokeWidth: 1
+      regionStyle: {
+        initial: {
+          fill: "#fdfbe5",
+          stroke: "#757674",
+          strokeWidth: 1
+        },
+        selected: {fill: "#e7f3ea"}
       },
-      selected: {fill: "#e7f3ea"}
-    },
 
-    onRegionSelected(code) {
-      // Persist selected region for Green/Red logic (see Back Template)
-      sessionStorage.setItem("selectedRegion", code);
+      onRegionSelected(code) {
+        // Persist selected region for Green/Red logic (see Back Template)
+        sessionStorage.setItem("selectedRegion", code);
 
-      // Show answer hack
-      //AnkiDroid requires use of API instead of event dispatch
-      if (typeof AnkiDroidJS !== "undefined") {
-        showAnswer();
-      } else {
-        // Simulate pressing "Enter" on the input element to show the answer
-        let input = document.querySelector("textarea#typeans");
-        let ev = new KeyboardEvent("keypress", {code: "Enter"});
-        input.dispatchEvent(ev);
-      }
-    },
+        // Show answer hack
+        //AnkiDroid requires use of API instead of event dispatch
+        if (typeof AnkiDroidJS !== "undefined") {
+          showAnswer();
+        } else {
+          // Simulate pressing "Enter" on the input element to show the answer
+          let input = document.querySelector("textarea#typeans");
+          let ev = new KeyboardEvent("keypress", {code: "Enter"});
+          input.dispatchEvent(ev);
+        }
+      },
 
-    ...mobileHack
-  });
+      ...mobileHack
+    });
+  }
+
+  function staticMapFallbackMode() {
+    document.querySelector(".value--image").style.display = "block";
+  }
 }())
